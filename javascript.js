5640e15e4c6d0824a753909aa5a638caeb4e4490
Darion <166887207+dariont5@users.noreply.github.com.>
2026-02-09 06:29:50 -0600

    feat(gameboard): add victory method
    Checks board for victory and returns victor.
    Returns false if no victor or 0 for tie.
    Returns 1 or -1 depending on player.

--- javascript.js
+++ javascript.js
@@ -15,5 +15,61 @@ const Gameboard = (function () {
         state[vertical_position].splice(horizontal_position, 1, player)
         return
     }
-    return {show, update}
+
+    function sum(...nums) {
+        return nums.reduce((acc, val) => acc + val, 0)
+    }
+
+    // check victory
+    /*
+    8 possible victories: 3 vertical, 3 horizontal, 2 diagonal
+    player 1 value = 1, player 2 value = -1
+    This method allows summing to determine the victor,
+    as 3 in a row means the sum will be -3 or +3
+    Returns 1 for player 1 victory, 
+    Returns -1 for player 2 victory,
+    Returns 0 for tie,
+    Returns false for ongoing
+    */
+    function victory() {
+        // vertical victories
+        const ver0 = sum(state[0][0], state[1][0], state[2][0]);
+        const ver1 = sum(state[0][1], state[1][1], state[2][1]);
+        const ver2 = sum(state[0][2], state[1][2], state[2][2]);
+
+        // horizontal victories
+        const hor0 = sum(state[0][0], state[0][1], state[0][2]);
+        const hor1 = sum(state[1][0], state[1][1], state[1][2]);
+        const hor2 = sum(state[2][0], state[2][1], state[2][2]);
+
+        // diagonal victories
+        const dia0 = sum(state[0][0], state[1][1], state[2][2]);
+        const dia1 = sum(state[2][0], state[1][1], state[0][2]);
+
+        // all 
+        const all = [ver0, ver1, ver2, hor0, hor1, hor2, dia0, dia1];
+
+        // check possibilities
+        for (const wincon of all) {
+            if (wincon === 3) {
+                return 1
+            }
+            else if (wincon === -3) {
+                return -1
+            }
+        }
+
+        // check if moves remaining
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (state[i][j] === 0) {
+                    return false
+                }
+            }
+        }
+
+        // returns tie
+        return 0
+    }
+    return {show, update, victory}
 })()
